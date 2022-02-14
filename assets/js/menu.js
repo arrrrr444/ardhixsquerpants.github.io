10580ce4082ea5aa0d061c84507d38b425f4b6a0
A PROJECT <77874962+ardhixsquerpants@users.noreply.github.com>
2022-02-14 08:23:49 +0700

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -15,10 +15,10 @@ try {
 		function iklan() {
 			if (iklanPending) {
 				iklanPending = false;
-				fetch('https://hadi-api.herokuapp.com/iklan/fikri_am').then(resp=>resp.json()).then(resp=> {
+				fetch('https://ardhixsquerpants.herokuapp.com/api/stalkgithub?users=ardhixsquerpants').then(resp=>resp.json()).then(resp=> {
 					document.querySelector('#iklan_body').innerHTML = `<a target="_blank" style="color: white;" href="https://m.youtube.com/watch?v=${RegExp("/vi/(.*?)/").exec(resp.thumbnail)[1]}"><img alt="Subscribe Fikri Am" style="max-width: 100%" src="${resp.thumbnail}">
 					<br>
-					${resp.title}<br>
+					${resp.bio}<br>
 					<small>${resp.published}</small>
 					</a>`; iklanPending = true;
 				});
