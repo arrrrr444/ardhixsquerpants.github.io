4f92fafe7d1111f0683756b3d7016019c431de64
A PROJECT <77874962+ardhixsquerpants@users.noreply.github.com>
2022-02-18 03:00:25 +0700

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -128,12 +128,12 @@ try {
 		//-- pengunjung visitor length
 		if (localStorage.getItem('Pengunjung')) {
 			setInterval(function() {
-				fetch("https://api.countapi.xyz/get/ardhixs-api-viewer/").then(res=>res.json()).then(res=> {
+				fetch("https://api.countapi.xyz/get/hadi-api-viewer/").then(res=>res.json()).then(res=> {
 					tag_pengunjung.innerHTML = res.value+" <small>perangkat</small>";
 				});
 			}, 2500);
 		} else {
-			fetch("https://api.countapi.xyz/hit/ardhixs-api-viewer/").then(res=>res.json()).then(res=> {
+			fetch("https://api.countapi.xyz/hit/hadi-api-viewer/").then(res=>res.json()).then(res=> {
 				localStorage.setItem('Pengunjung', 'true');
 				tag_pengunjung.innerHTML = res.value+" <small>perangkat</small>";
 			});
